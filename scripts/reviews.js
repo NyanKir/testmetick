87cfd3af50d6c97435c13664aee4a8a3beba1708
Kirill Sytnyanskiy <nyan.kirill@gmail.com>
2021-11-05 18:37:09 +0300

    asdasda

---
+++ scripts/reviews.js
@@ -0,0 +1,23 @@
+{
+    "@context": "https://schema.org/",
+    "@type": "Review",
+    "itemReviewed": {
+    "@type": "Organization",
+        "image": "http://www.example.com/seafood-restaurant.jpg",
+        "name": "Legal Seafood",
+
+},
+    "reviewRating": {
+    "@type": "Rating",
+        "ratingValue": "5"
+},
+    "name": "A good seafood place.",
+    "author": {
+    "@type": "Person",
+        "name": "Bob Smith"
+},
+    "publisher": {
+    "@type": "Organization",
+        "name": "MediasScan"
+}
+}
